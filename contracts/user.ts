63a228c8c82a056360ce6789cfed63ab46f9b43f
zeworks <josenogueira95@hotmail.com>
2023-05-28 02:02:50 +0100

    use pnpm + update user contract

--- contracts/user.ts
+++ contracts/user.ts
@@ -1,45 +1,56 @@
 import { z } from "zod"
 import { Base } from "./base"
 
-export const UserRole = z.intersection(
+//#region error messages
+export const ERROR_USER_ROLE_NAME_REQUIRED = "field name is required"
+export const ERROR_USER_EMAIL_REQUIRED = "field email is required"
+export const ERROR_USER_EMAIL_INVALID = "this is not a valid email"
+export const ERROR_USER_FIRSTNAME_MINLENGTH =
+	"first name must contain at least 2 characters"
+export const ERROR_USER_LASTNAME_MINLENGTH =
+	"last name must contain at least 2 characters"
+export const ERROR_USER_USERNAME_MINLENGTH =
+	"username must contain at least 4 characters"
+//#endregion error messages
+
+export const Role = z.intersection(
 	Base,
 	z.object({
-		name: z.string({ required_error: "Name is required" }).optional(),
+		name: z
+			.string({ required_error: ERROR_USER_ROLE_NAME_REQUIRED })
+			.optional(),
 		key: z.string().optional(),
 	})
 )
 
-export const UserProfile = z.object({
-	first_name: z
-		.string()
-		.min(2, "first name must contain at least 2 characters"),
-	last_name: z
-		.string()
-		.min(2, "last name must contain at least 2 characters")
-		.optional(),
+export const Profile = z.object({
+	first_name: z.string().min(2, ERROR_USER_FIRSTNAME_MINLENGTH),
+	last_name: z.string().min(2, ERROR_USER_LASTNAME_MINLENGTH).optional(),
 	picture: z.string().optional(),
 })
 
-export const User = z.intersection(
-	Base,
+export const Status = z.enum(["ACTIVE", "INACTIVE", "BLOCKED"])
+
+export const User = Base.merge(
 	z.object({
-		username: z.string(),
-		email: z.string(),
+		username: z.string().min(4, ERROR_USER_USERNAME_MINLENGTH),
+		email: z
+			.string()
+			.min(1, ERROR_USER_EMAIL_REQUIRED)
+			.email(ERROR_USER_EMAIL_INVALID),
 		password: z.string().optional(),
-		profile: UserProfile,
+		profile: Profile,
+		access_token: z.string().optional(),
+		status: Status.default("INACTIVE").optional(),
 	})
 )
 
-export const CreateUserData = z.object({
-	username: z.string().min(4, "username must contain at least 4 characters"),
-	email: z
-		.string()
-		.min(1, "This field has to be filled.")
-		.email("this is not a valid email"),
-	password: z.string().optional(),
-	profile: UserProfile,
-})
+export const CreateUserData = User.omit({ id: true, createdAt: true })
+export const UpdateUserData = User
 
 export type User = z.TypeOf<typeof User>
-export type UserProfile = z.TypeOf<typeof UserProfile>
+export type Status = z.TypeOf<typeof Status>
+export type Profile = z.TypeOf<typeof Profile>
+export type Role = z.TypeOf<typeof Role>
 export type CreateUserData = z.TypeOf<typeof CreateUserData>
+export type UpdateUserData = z.TypeOf<typeof UpdateUserData>
