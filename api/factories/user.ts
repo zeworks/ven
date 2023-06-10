941d378caa1bb3dde5da57f126ea59d2e5f85080
zeworks <josenogueira95@hotmail.com>
2023-06-10 03:11:40 +0100

    add create user validation

--- api/factories/user.ts
+++ api/factories/user.ts
@@ -1,5 +1,8 @@
+import { CreateUserData } from "../../contracts/user"
 import { uuid } from "../adapters/uuid"
+import { validationErrorHandler } from "../adapters/validationErrorHandler"
 import { createUserController } from "../controllers/user"
+import { Validation } from "../domain/protocols/validation"
 import { userRepository } from "../repositories/user"
 import { userMemoryRepository } from "../repositories/user.memory"
 import { createUserUsecase } from "../usecases/user"
@@ -9,9 +12,15 @@ export const createUserUsecaseFactoryMemory = createUserUsecase(
 	userMemoryRepository,
 	uuid
 )
+export const createUserValidation: Validation = (input) => {
+	const result = CreateUserData.safeParse(input)
+	if (!result.success) return validationErrorHandler(result)
+}
 export const createUserControllerFactory = createUserController(
-	createUserUsecaseFactory
+	createUserUsecaseFactory,
+	createUserValidation
 )
 export const createUserControllerFactoryMemory = createUserController(
-	createUserUsecaseFactoryMemory
+	createUserUsecaseFactoryMemory,
+	createUserValidation
 )
