3c49fa8d60067e41a3ad20b437acf71f21a9e4dc
zeworks <josenogueira95@hotmail.com>
2023-10-02 18:10:45 +0100

    install shadcnui

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css"
+import { Button } from "@/components/ui/button"
 
 function App() {
-	return <div>hee</div>
+	return (
+		<div>
+			hee
+			<Button>teste</Button>
+		</div>
+	)
 }
 
 export default App
