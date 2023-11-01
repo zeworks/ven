471a5e1eac7c93afed3002b23949b7b812c3abe2
zeworks <josenogueira95@hotmail.com>
2023-11-01 22:48:13 +0000

    add fastify server

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,23 +1,6 @@
-import Fastify from "fastify"
-import mercurius from "mercurius"
-import resolvers from "./graphql/resolvers"
-import schema from "./graphql/schema"
-import { makeExecutableSchema } from "@graphql-tools/schema"
-import "dotenv/config"
+import app, { HTTP_PORT } from "./app"
 
-const HTTP_PORT = process.env.PORT || 4000
-
-const app = Fastify({
-	logger: process.env.NODE_ENV !== "production",
-})
-
-app.register(mercurius, {
-	schema: makeExecutableSchema({ typeDefs: schema }),
-	resolvers,
-	graphiql: process.env.NODE_ENV === "development" ? "graphiql" : false,
-})
-
-const start = () => {
+const startServer = () => {
 	try {
 		app.listen({ port: HTTP_PORT as number })
 	} catch (err) {
@@ -25,4 +8,5 @@ const start = () => {
 		process.exit(1)
 	}
 }
-start()
+
+startServer()
